6b69b6780b16d48f5bfad91804a0608fa98203c6
Terence Ng <awesome.terence@gmail.com>
2013-05-27 23:31:35 +0800

    Delete header

--- header/read_files.h
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-/****************************************
- * define how to read lines or characters
- * from files outside.
- * **************************************/
-
-
