4a38a969615467a94db3a044e91bc3311f6628bb
Terence Ng <awesome.terence@gmail.com>
2013-05-29 13:26:36 +0800

    Read from the outside files

--- header/read_files.h
+++ header/read_files.h
@@ -2,9 +2,10 @@
 #include<stdlib.h>
 
 /****************************************
- * define how to read lines or characters
+ * "read_files.h"
+ * defines how to read lines or characters
  * from files outside.
- * **************************************/
+ ****************************************/
 
 struct identifier_table {
 	char *value;
@@ -22,10 +23,45 @@ struct reserved_table {
 struct reserved_table reserved_table_array[20];
 
 char c;
+char line_buffer[100];
 
-void read_a_line(FILE *fp) {
-	while((c=fgetc(fp))!=EOF) {
+int i;
+
+void allocation() {
+	for(i=0; i<50; i++) {
+		identifier_table_array[i].value = (char *)malloc(100);
+	}
+	for(i=0; i<20; i++) {
+		reserved_table_array[i].reserved_name = (char *)malloc(20);
+		reserved_table_array[i].value = (char *)malloc(20);
+	}
+}
+
+void read_config_file(char *path_to_config) {
+	FILE *config_file_pointer;
+
+	if(!(config_file_pointer=fopen(path_to_config, "r"))) {
+		printf("Error openning configure file!\n");
+		exit(1);
+	}
+
+	while((c=fgetc(config_file_pointer))!=EOF) {
 		printf("%c", c);
+	}
+
+	if(fclose(config_file_pointer)) {
+		printf("Error closing configure file!\n");
+		exit(1);
+	}
+}
+
+void free_space() {
+	for(i=0; i<50; i++) {
+		free(identifier_table_array[i].value);
+	}
+	for(i=0; i<20; i++) {
+		free(reserved_table_array[i].reserved_name);
+		free(reserved_table_array[i].value);
 	}
 }
 
